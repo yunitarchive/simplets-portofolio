9f02e620c82974d4547d67839976c3b42bcfaebf
feny <feny>
2024-09-30 15:09:12 +0700

    edit scroll gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,34 +6,34 @@ interface GalleryProps {
 }
 
 interface GalleryContainerProps {
-  items: GalleryProps[]; 
+  items: GalleryProps[];
 }
 
 const Gallery: FC<GalleryContainerProps> = ({ items }) => {
-    const half = Math.ceil(items.length / 2);
-    const itemsFirst = items.slice(0, half);
-    const itemsSecond = items.slice(half); 
+  const half = Math.ceil(items.length / 2);
+  const itemsFirst = items.slice(0, half);
+  const itemsSecond = items.slice(half);
+
   return (
     <div className="space-y-6">
-    <div className="w-full mx-20 justify-end">
-      <div className="flex space-x-10 snap-x snap-mandatory">
-        {itemsFirst.map((item, index) => (
-          <a
-            href={item.link}
-            key={index}
-            className="snap-center flex-shrink-0"
-          >
-            <div className="bg-gray-400 w-[360px] rounded-lg h-[280px] p-9 flex items-center justify-center">
-              <img
-                src={item.cover}
-                className="object-cover w-full h-full"
-              />
-            </div>
-          </a>
-        ))}
+   
+      <div className="w-full overflow-x-auto mx-20">
+        <div className="flex space-x-10">
+          {itemsFirst.map((item, index) => (
+            <a
+              href={item.link}
+              key={index}
+              className="snap-center flex-shrink-0"
+            >
+              <div className="bg-gray-400 w-[360px] rounded-lg h-[280px] p-9 flex items-center justify-center">
+                <img src={item.cover} className="object-cover w-full h-full" />
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
-        <div className="w-full mx-20 justify-start">
+
+      <div className="w-full overflow-x-auto mx-20">
         <div className="flex space-x-10 snap-x snap-mandatory">
           {itemsSecond.map((item, index) => (
             <a
@@ -42,16 +42,13 @@ const Gallery: FC<GalleryContainerProps> = ({ items }) => {
               className="snap-center flex-shrink-0"
             >
               <div className="bg-gray-400 w-[360px] rounded-lg h-[280px] p-9 flex items-center justify-center">
-                <img
-                  src={item.cover}
-                  className="object-cover w-full h-full"
-                />
+                <img src={item.cover} className="object-cover w-full h-full" />
               </div>
             </a>
           ))}
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -29,7 +29,7 @@ const LandingPage: FC = () => {
        
    <section id="cover" className="min-h-full">
       <Header  bgColor="bg-gray-400" />
-      <Hero profilePhoto={ ProfilelPhoto } profileCaption="Webflow Developer - UI/UX Designer"  profileGreet="Hello, I'm Ayush" />
+      <Hero profilePhoto={ ProfilelPhoto } profileCaption="flow Developer - UI/UX Designer"  profileGreet="Hello, I'm Ayush" />
       </section>
       <section id="about" className="relative bg-neutral-400">
         <Banner title="About" description={descAbout}/>
